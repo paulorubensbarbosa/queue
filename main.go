2c2bb10a3b381e06f3fd11a32c28ab6dbe9ee8c3
Paulo Rubens Barbosa <paulorubensbarbosa@outlook.com>
2020-11-17 14:14:49 -0300

    The Queue has been done

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 import "fmt"
+import "math"
 //Creating a Queue Structure
 
 type Queue struct {
@@ -17,10 +18,9 @@ func newQueue (capacity int)*Queue{
 	queue.size 		= 0
 	queue.rear 		= capacity - 1
 	queue.slice = make([]int, capacity)
-	fmt.Println(queue.slice)
 	return queue	
 }
-
+//functions for verification
 func isFull(queue *Queue) bool{
 	return(queue.size==queue.capacity)
 }
@@ -28,7 +28,7 @@ func isFull(queue *Queue) bool{
 func isEmpty(queue *Queue) bool{
 	return(queue.size==0)
 }
-
+//Function to enqueue, this add 1 item int tipe, add + 1 to queue.rear and add+ 1 to queue size
 func enqueue(queue *Queue, item int){
 	if(isFull(queue)){
 		return;
@@ -36,14 +36,48 @@ func enqueue(queue *Queue, item int){
 		queue.rear=(queue.rear+1)%queue.capacity
 		queue.slice[queue.rear]=item
 		queue.size=queue.size+1
-		fmt.Println(item," Foi enfileirado")
-		fmt.Println(queue.slice)
+		fmt.Println("item: ", item," enqueued to queue")
+	}
+}
+//Function to dequeue, this remove 1 item int tipe(the first of the queue), add + 1 to the queue front and remove -1 to de queue size
+func dequeue(queue *Queue) int{
+	if(isEmpty(queue)){
+		return math.MinInt16
+	}else{
+		var item int = queue.slice[queue.front]
+		queue.front = (queue.front + 1)%queue.capacity
+		queue.size=queue.size-1
+		return item
+	}
+}
+// functions for show the intem at the front and at the rear of the queue
+func getFront(queue *Queue)int{
+	if(isEmpty(queue)){
+		return math.MinInt16
+	}else{
+		return queue.slice[queue.front]
 	}
 }
 
+func getRear(queue *Queue)int{
+	if(isEmpty(queue)){
+		return math.MinInt16
+	}else{
+		return queue.slice[queue.rear]
+	}
+}
 
+//testing all functions
 func main() {
 var queue *Queue = newQueue(10)
 enqueue(queue, 1)
+enqueue(queue, 2)
+enqueue(queue, 3)
+
+fmt.Println ("item: ", dequeue(queue), " Dequeued from queue" )
+fmt.Println ("item: ", dequeue(queue), " Dequeued from queue" )
+
+fmt.Println("Front item is: ", getFront(queue))
+fmt.Println("Rear item is: ", getRear(queue))
 
 }
