371e775b7634c90b973745bd05985bd10f0a7576
Paulo Rubens Barbosa <paulorubensbarbosa@outlook.com>
2020-11-17 00:52:21 -0300

    enqueue function has been added

--- main.go
+++ main.go
@@ -10,19 +10,40 @@ type Queue struct {
 
 //function to create a Queue
 
-func newQueue(capacity int) {
+func newQueue (capacity int)*Queue{
 	queue 			:= new(Queue)
 	queue.capacity 	= capacity
 	queue.front 	= 0
 	queue.size 		= 0
 	queue.rear 		= capacity - 1
 	queue.slice = make([]int, capacity)
-	
-	fmt.Println("item no slice: ", queue.slice[0])
-	fmt.Println("cap: ", cap(queue.slice))
+	fmt.Println(queue.slice)
+	return queue	
+}
+
+func isFull(queue *Queue) bool{
+	return(queue.size==queue.capacity)
+}
+
+func isEmpty(queue *Queue) bool{
+	return(queue.size==0)
+}
+
+func enqueue(queue *Queue, item int){
+	if(isFull(queue)){
+		return;
+	}else{
+		queue.rear=(queue.rear+1)%queue.capacity
+		queue.slice[queue.rear]=item
+		queue.size=queue.size+1
+		fmt.Println(item," Foi enfileirado")
+		fmt.Println(queue.slice)
+	}
 }
 
 
 func main() {
-	newQueue(10)
+var queue *Queue = newQueue(10)
+enqueue(queue, 1)
+
 }
